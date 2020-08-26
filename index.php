7807a037b6f6f0ebb39fc6f104f0d9605e631840
alexangria1992 <alex040892@gmail.com>
2020-08-26 16:55:50 +1200

    innitial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="top">
+        <div class="container">
+            <div class="col-md-6 offer">
+                <a href="" class="btn btn-success btn-sm">
+                    Welcome: Guest
+                </a>
+                <a href="">
+                    Shopping Cart Total Price: $100, Total Items: 2
+                </a>
+            </div>
+            <div class="col-md-12">
+                <ul class="menu">
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go to Cart</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
