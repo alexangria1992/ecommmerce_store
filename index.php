8a29aba4750fcce152bc9d38751bf7639c5e87b4
alexangria1992 <alex040892@gmail.com>
2020-08-26 17:22:54 +1200

    fixed navbar header and added carousel section

--- index.php
+++ index.php
@@ -92,8 +92,46 @@
                             <input type="text" placeholder="Search" name="user_query" class="form-control" required>
                             <button class="btn btn-primary" type="submit" value="Search" name="search">
                                 <i class="fa fa-search"></i>
-                            </button>
+                                </button>
+                        </div>
+                        </form>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <!-- Carousel Section -->
+    <div class="container" id="slider">
+        <div class="col-md-12">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" class="active"  data-slide-to="0"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="admin_area/slides_images/1.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/slides_images/2.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/slides_images/3.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="admin_area/slides_images/4.jpg">
+                    </div>
+                </div>
+                <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a href="#myCarousel" class="right carousel-control" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
     </div>
