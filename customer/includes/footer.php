efd037f55bce8fe4cc6cfd02c51ac33c5bd4970f
alexangria1992 <alex040892@gmail.com>
2020-08-26 23:05:05 +1200

    included db and functions in all files that were missing it

--- customer/includes/footer.php
+++ customer/includes/footer.php
@@ -20,11 +20,16 @@
             <div class="col-md-3 col-sm-6">
                 <h4>Top Products Categories</h4>
                 <ul>
-                    <li><a href="">Jackets</a></li>
-                    <li><a href="">Accessories</a></li>
-                    <li><a href="">Shoes</a></li>
-                    <li><a href="">Coats</a></li>
-                    <li><a href="">T-Shirts</a></li>
+                <?php
+                    $get_p_cats = "SELECT * FROM product_categories";
+                    $run_p_cats = mysqli_query($con, $get_p_cats);
+                    while($row_p_cats = mysqli_fetch_array($run_p_cats))
+                    {
+                        $p_cat_id = $row_p_cats['p_cat_id'];
+                        $p_cat_title = $row_p_cats['p_cat_title'];
+                        echo "<li><a href='shop.php?p_cat=$p_cat_id'>$p_cat_title</a></li>";
+                    }
+                    ?>
                 </ul>
                 <hr class="hidden-md hidden-lg">
             </div>
