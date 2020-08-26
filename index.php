dbbcc77df871f80d49d70bb386d9d5c14829fce2
alexangria1992 <alex040892@gmail.com>
2020-08-26 17:07:28 +1200

    fixed col error and created first navigation and began second navigation

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet">
     <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
@@ -19,7 +21,7 @@
                     Shopping Cart Total Price: $100, Total Items: 2
                 </a>
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <ul class="menu">
                     <li>
                         <a href="customer_register.php">Register</a>
@@ -34,6 +36,30 @@
                         <a href="login.php">Login</a>
                     </li>
                 </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="navbar navbar-default" id="navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand home">
+                    <img src="images/logo.png" alt="computerfever logo" class="hidden-xs">
+                    <img src="images/logo-small.png" alt="computerfever logo" class="visible-xs">
+                </a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-align-justify"></i>
+                </button>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                    <span class="sr-only">Toggle Search</span>
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navigation">
+                <div class="padding-nav">
+                    
+                </div>
             </div>
         </div>
     </div>
