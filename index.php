b524b4b39ccadaf928236cf981e26681ca765531
alexangria1992 <alex040892@gmail.com>
2020-08-26 20:52:34 +1200

    created slider table in phpmyadmin and created dynamic carousel and t…  …hen created products table in phpmyadmin

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    include("includes/db.php");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -111,18 +116,38 @@
                     <li data-target="#myCarousel" data-slide-to="3"></li>
                 </ol>
                 <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="admin_area/slides_images/1.jpg">
-                    </div>
-                    <div class="item">
-                        <img src="admin_area/slides_images/2.jpg">
-                    </div>
-                    <div class="item">
-                        <img src="admin_area/slides_images/3.jpg">
-                    </div>
-                    <div class="item">
-                        <img src="admin_area/slides_images/4.jpg">
-                    </div>
+                <?php
+                        $get_slides = "SELECT * FROM slider LIMIT 0, 1";
+                        $run_slides = mysqli_query($con, $get_slides);
+
+                        while($row_slides = mysqli_fetch_array($run_slides))
+                        {
+                            $slide_name = $row_slides['slide_name'];
+                            $slide_image = $row_slides['slide_image'];
+
+                            echo "
+                            <div class='item active'>
+                                <img src='admin_area/slides_images/$slide_image'>
+                            </div>
+                            ";
+                        }
+                    ?>
+                    <?php
+                         $get_slides = "SELECT * FROM slider LIMIT 1, 3";
+                         $run_slides = mysqli_query($con, $get_slides);
+
+                         while($row_slides = mysqli_fetch_array($run_slides))
+                         {
+                            $slide_name = $row_slides['slide_name'];
+                            $slide_image = $row_slides['slide_image'];
+
+                            echo "
+                            <div class='item'>
+                                <img src='admin_area/slides_images/$slide_image'>
+                            </div>
+                            ";
+                         }
+                    ?>
                 </div>
                 <a href="#myCarousel" class="left carousel-control" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left"></span>
