8f7040e3ad99bd55b894a09649a933180d3d8bf4
alexangria1992 <alex040892@gmail.com>
2020-08-26 22:22:56 +1200

    Successfully inserted products into database

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -12,6 +12,17 @@
     <title>Insert Products</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://cdn.tiny.cloud/1/siujnhf3bvynlz3szar255phnzjlqoamqz6g56put5sbiipu/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+      toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Author name',
+    });
+  </script>
 </head>
 <body>
 
@@ -63,30 +74,72 @@
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-3 control-label">Product Title</label>
+                            <label class="col-md-3 control-label"> Category</label>
                             <div class="col-md-6">
-                                <input type="text" name="product_title" class="form-control">
+                                <select name="cat" class="form-control">
+                                    <option>Select a Category</option>
+                                    <?php
+                                        $get_cat = "SELECT * FROM categories";
+                                        $run_cat = mysqli_query($con, $get_cat);
+                                        while($row_cat = mysqli_fetch_array($run_cat))
+                                        {
+                                            $cat_id = $row_cat['cat_id'];
+                                            $cat_title = $row_cat['cat_title'];
+
+                                            echo "<option value='$cat_id'>$cat_title</option>";
+                                        }
+
+                                    ?>
+                                </select>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-3 control-label">Product Title</label>
+                            <label class="col-md-3 control-label">Product Image 1</label>
                             <div class="col-md-6">
-                                <input type="text" name="product_title" class="form-control">
+                                <input type="file" name="product_img1" class="form-control" required>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-3 control-label">Product Title</label>
+                            <label class="col-md-3 control-label">Product Image 2</label>
                             <div class="col-md-6">
-                                <input type="text" name="product_title" class="form-control">
+                                <input type="file" name="product_img2" class="form-control" required>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-3 control-label">Product Title</label>
+                            <label class="col-md-3 control-label">Product Image 3</label>
                             <div class="col-md-6">
-                                <input type="text" name="product_title" class="form-control">
+                                <input type="file" name="product_img3" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Price</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_price" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Keywords</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_keywords" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Description</label>
+                            <div class="col-md-6">
+                            <textarea name="product_desc" class="form-control" rows="6" cols="19"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label"></label>
+                            <div class="col-md-6">
+                            <input type="submit" name="submit" value="Insert Product" class="btn btn-primary form-control" required>
                             </div>
                         </div>
                    </form>
@@ -100,3 +153,41 @@
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
+<?php
+   if(isset($_POST['submit']))
+   {
+       $product_title = $_POST['product_title'];
+       $product_cat = $_POST['product_cat'];
+       $cat = $_POST['cat'];
+       $product_price = $_POST['product_price'];
+       $product_desc = $_POST['product_desc'];
+       $product_keywords = $_POST['product_keywords'];
+  
+       $product_img1 = $_FILES['product_img1']['name'];
+       $product_img2 = $_FILES['product_img2']['name'];
+       $product_img3 = $_FILES['product_img3']['name'];
+  
+       $temp_name1 = $_FILES['product_img1']['tmp_name'];
+       $temp_name2 = $_FILES['product_img2']['tmp_name'];
+       $temp_name3 = $_FILES['product_img3']['tmp_name'];
+  
+       move_uploaded_file($temp_name1, "product_images/$product_img1");
+       move_uploaded_file($temp_name2, "product_images/$product_img2");
+       move_uploaded_file($temp_name3, "product_images/$product_img3");
+  
+       $insert_product = "INSERT INTO products (p_cat_id, cat_id, date, product_title, product_img1, product_img2, product_img3, 
+       product_price, product_desc, product_keywords) VALUES ('$product_cat', '$cat', NOW(), '$product_title', '$product_img1', '$product_img2', 
+       '$product_img3', '$product_price', '$product_desc', '$product_keywords')";
+  
+       $run_product = mysqli_query($con, $insert_product);
+  
+       if($run_product)
+       {
+          echo "<script>alert('Product has been inserted successfully')</script>";
+          echo "<script>window.open('insert_product.php', '_self')</script>";
+       }
+  
+  
+   }
+?>
