2327b10ca36ae86828574ad35e041aecfcd671d5
alexangria1992 <alex040892@gmail.com>
2020-08-26 21:18:30 +1200

    copied files from root folder into admin_area and linked them in insert_product.php

---
+++ admin_area/insert_product.php
@@ -0,0 +1,21 @@
+<?php
+    include("includes/db.php");
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Products</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
