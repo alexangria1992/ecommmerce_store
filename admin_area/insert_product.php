1a3879d73aac09062574eadac39ade0c46de488c
alexangria1992 <alex040892@gmail.com>
2020-08-26 21:22:03 +1200

    Began insert_product.php in admin_area.php

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -14,6 +14,34 @@
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
+
+<div class="row">
+    <div class="col-lg-12">
+        <ol class="breadcrumb">
+            <li class="active">
+                <i class="fa fa-dashboard"></i> Dashboard / Insert Products
+            </li>
+        </ol>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    <i class="fa fa-money fa-fw"></i>Insert Products
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+
+                   </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
