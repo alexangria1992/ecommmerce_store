350b8e1037f5864ee694adaf70174e7e6b1a299f
alexangria1992 <alex040892@gmail.com>
2020-08-26 21:23:46 +1200

    Fetched from product categories in dropdown select

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -35,7 +35,60 @@
                 </div>
                 <div class="panel-body">
                     <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                    <div class="form-group">
+                            <label class="col-md-3 control-label">Product Title</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_title" class="form-control">
+                            </div>
+                        </div>
 
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Category</label>
+                            <div class="col-md-6">
+                                <select name="product_cat" class="form-control">
+                                    <option>Select a Product Category</option>
+                                    <?php
+                                        $get_p_cats = "SELECT * FROM product_categories ";
+                                        $run_p_cats = mysqli_query($con, $get_p_cats);
+                                        while($row_p_cats = mysqli_fetch_array($run_p_cats))
+                                        {
+                                            $p_cat_id = $row_p_cats['p_cat_id'];
+                                            $p_cat_title = $row_p_cats['p_cat_title'];
+
+                                            echo "<option value='$p_cat_id'>$p_cat_title</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Title</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_title" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Title</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_title" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Title</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_title" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-3 control-label">Product Title</label>
+                            <div class="col-md-6">
+                                <input type="text" name="product_title" class="form-control">
+                            </div>
+                        </div>
                    </form>
                 </div>
             </div>
