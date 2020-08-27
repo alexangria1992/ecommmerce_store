cf71126f3d9d7f162f5e5c934995e13a51b13d68
alexangria1992 <alex040892@gmail.com>
2020-08-27 14:34:30 +1200

    dynamically created product categories

--- functions/functions.php
+++ functions/functions.php
@@ -161,7 +161,36 @@ function getcatpro()
         <p>$cat_desc</p>
         </div>
         ";
-    }
+      }
+
+      while($row_products = mysqli_fetch_array($run_products))
+      {
+        $pro_id = $row_products['product_id'];
+        $pro_title = $row_products['product_title'];
+        $pro_price = $row_products['product_price'];
+        $pro_desc = $row_products['product_desc'];
+        $pro_img1 = $row_products['product_img1'];
+
+        echo "
+        <div class='col-md-4 col-sm-6 center-responsive'>
+            <div class='product'>
+            <a href = 'details.php?pro_id=$pro_id'>
+            <img src='admin_area/product_images/$pro_img1' class='img-responsive'>
+            </a>
+            <div class='text'>
+            <h3><a href='details.php?pro_id=$pro_id'>$pro_title</a></h3>
+            <p class='price'>$ $pro_price</p>
+            <p class='buttons'>
+            <a href='details.php?pro_id=$pro_id' class='btn btn-default'>View Details</a>
+            <a href='details.php?pro_id=$pro_id' class='btn btn-primary'>
+            <i class='fa fa-shopping-cart'></i> Add To Cart</a>
+            </p>
+            </div>
+          </div>
+        </div>
+        ";
+
+      }
 
     }
 }
